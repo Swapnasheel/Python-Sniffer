c08ebb2f118d382eb19e38ec59022edc7ad7bc89
Swapnasheel <swar.1318@gmail.com>
2018-03-20 07:38:47 +0000

    #4 Added TCP flags

--- sniffer.py
+++ sniffer.py
@@ -50,6 +50,16 @@ def icmp_packet(data):
 def tcp_packets(data):
     (src_port, dst_port, seq, ack, offset_reserved_flags) = struct.unpack("! H H L L H", data[:14])
     offset = (offset_reserved_flags >> 12) * 4
+    flag_urg = (offset_reserved_flags & 32) >> 5)
+    flag_ack = (offset_reserved_flags & 16) >> 4)
+    flag_push = (offset_reserved_flags & 8) >> 3)
+    flag_rst = (offset_reserved_flags & 4) >> 2)
+    flag_syn = (offset_reserved_flags & 2) >> 1)
+    flag_fin = offset_reserved_flags & 1
+
+    return src_port, dst_port, seq, ack,
+           flag_urg, flag_ack, flag_push, flag_rst, flag_syn, flag_fin
+
 
 
 
