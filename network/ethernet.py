580db92d3fbafa67d8b30a3fc5382f4bdc2689ac
Swapnasheel <swar.1318@gmail.com>
2018-03-20 08:03:31 +0000

    New approch, class commit #1

---
+++ network/ethernet.py
@@ -0,0 +1,19 @@
+import socket
+import struct
+
+class Ethernet():
+
+    def __init__(self, raw_data):
+  
+        dest_mac, src_mac, proto = struct.unpack("! 6s 6s H", data[:14])
+
+        self.dest_mac = get_mac_addr(dest_mac)
+        self.src_mac = get_mac_addr(src_mac)
+        self.proto = socket.htons(proto)
+        self.data = data[14:]
+
+    def get_mac_addr(mac):
+        byte_str = map('{:02x}'.format, mac)
+        return ':'.join(byte_str).upper()
+
+
